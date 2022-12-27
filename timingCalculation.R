c9d32e13e738155f2595ae93ba428d0d7e053471
Simon <simon@elisya.org>
2022-12-27 15:30:43 +0000

    update calculation play stuff

--- timingCalculation.R
+++ timingCalculation.R
@@ -1,3 +1,5 @@
+
+#write a list with 1 the date of start, 2 the total length time and 3 the list of element.
 alltimings=read.csv("octnov.csv")
 byday=sapply(unique(alltimings$date),function(d){
                  timings=alltimings[alltimings$date==d,]
@@ -62,8 +64,12 @@ byday=sapply(unique(encountertimings$date),function(d){
                  sum(tot/60)
 })
 
+timings=read.csv("timesheet_202211.csv",header=F)
+timings=read.csv("timesheet_202212.csv",header=F)
+colnames(timings)=c("date","time","type","notes")
+
 tot=c()
-timings=read.csv("octnov.csv")
+timinglist=list()
 for(i in seq(1,nrow(timings)-1,2)){
     h1=floor(timings[i+1,"time"]/100)
     h2=floor(timings[i,"time"]/100)
@@ -75,5 +81,30 @@ for(i in seq(1,nrow(timings)-1,2)){
     m2=timings[i,"time"]-h2*100
     minutes=(m1-m2)
     total=hours+minutes
+    timinglist[[paste0(timings[i,"date"],timings[i+1,"time"])]]=list ( length=total, check=c(timings[i,"type"],timings[i+1,"type"]), cat=paste0(unique(toupper(c(timings[i,"notes"],timings[i+1,"notes"]))),collapse=" "))
     tot=c(tot,total)
 }
+
+durations=sapply(timinglist,"[[","length")
+allcats=sapply(timinglist,"[[","cat")
+allcats=strsplit(allcats,"[/|+ ]") 
+allcats=lapply(allcats,unique)
+
+
+
+
+c(
+normal=sum(durations[
+ grepl("ENCO",lapply(timinglist,"[[","cat")) |
+ grepl("CDAL",lapply(timinglist,"[[","cat")) |
+ grepl("REVIEW",lapply(timinglist,"[[","cat")) |
+ grepl("BA ",lapply(timinglist,"[[","cat")) |
+ grepl("GENERAL",lapply(timinglist,"[[","cat")) |
+ grepl("GRANT",lapply(timinglist,"[[","cat")) |
+ grepl("ADMIN",lapply(timinglist,"[[","cat")) ])/60,
+
+
+    bentley=sum(durations[grepl("BENT",lapply(timinglist,"[[","cat")) ])/60,
+    network=sum(durations[grepl("NETW",lapply(timinglist,"[[","cat")) ])/60,
+    depofarch=sum(durations[grepl("DEPOFARCH",lapply(timinglist,"[[","cat")) ])/60
+)
