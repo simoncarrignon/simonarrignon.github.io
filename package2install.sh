118c81ca57abbd95a976bc49bad899007b3cb0d6
Simon <simon@elisya.org>
2022-12-30 09:59:08 +0100

    add gtkvim for global copy paste

--- package2install.sh
+++ package2install.sh
@@ -1,1 +1,2 @@
-apt install audacity pass neomutt tmux vim git yakuake curl r-base whois nmap pip pinentry-tty
+apt install audacity pass neomutt tmux vim-gtk3 git yakuake curl r-base whois nmap pip pinentry-tty 
+#vim gtk3 is to be able to opy paste
